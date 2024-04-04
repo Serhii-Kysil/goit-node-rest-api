75495a18978859d75ffb6ed40ca37f9b75223b84
Serhii-Kysil <serik.kisel000@gmail.com>
2024-04-04 17:14:48 +0300

    Add start task

---
+++ services/contactsServices.js
@@ -0,0 +1,26 @@
+import Contact from "../models/Contact.js";
+
+export const listContacts = () => {
+  return Contact.find();
+};
+export async function getContactById(id) {
+  return Contact.findById(id);
+}
+
+export async function removeContact(id) {
+  return Contact.findByIdAndDelete(id);
+}
+
+export async function addContact(body) {
+  return Contact.create(body);
+}
+export async function updateContact(id, body) {
+  return Contact.findByIdAndUpdate(id, body, {
+    new: true,
+  });
+}
+export async function updateStatus(id, body) {
+  return Contact.findByIdAndUpdate(id, body, {
+    new: true,
+  });
+}
